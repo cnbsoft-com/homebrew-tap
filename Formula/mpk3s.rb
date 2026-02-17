b6cac56edded0dd306e589f912d80c9816b0e4e9
dino <dino@cnbsoft.com>
2026-02-17 13:41:01 +0900

    homebrew 설정 추가

---
+++ Formula/mpk3s.rb
@@ -0,0 +1,19 @@
+class Mpk3s < Formula
+  desc "Multipass based K3s cluster management CLI"
+  homepage "https://github.com/cnbsoft-com/k3s-helper"
+  url "https://github.com/cnbsoft-com/k3s-helper/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "a8176f92a9b32f80eca266391bc16c7b6093716eb4bd0b2b7a0ea9bafc98ff74"
+  license "MIT"
+
+  depends_on "multipass"
+
+  def install
+    bin.install "bin/mpk3s"
+    # bin.install "libs/k3s-helper"
+    # libexec.install Dir["libs/*.sh"]
+  end
+
+  test do
+    system "#{bin}/mpk3s", "usage"
+  end
+end
